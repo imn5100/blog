84717b768e087f9e6caf75245bb02d08ff2bae9a
tuanzi <tuanzi@2dfire.com>
2016-09-06 16:52:58 +0800

    update

--- src/main/java/com/shaw/controller/IndexController.java
+++ src/main/java/com/shaw/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.shaw.service.BlogService;
 import com.shaw.util.PageBean;
 import com.shaw.util.PageUtil;
 import com.shaw.util.StringUtil;
+import com.shaw.util.TimeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,69 +25,72 @@ import java.util.Map;
 @RequestMapping("/")
 public class IndexController {
 
-	@Resource
-	private BlogService blogService;
-	
-	
-	/**
-	 * 请求主页
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/index")
-	public ModelAndView index(@RequestParam(value="page",required=false)String page,@RequestParam(value="typeId",required=false)String typeId,@RequestParam(value="releaseDateStr",required=false)String releaseDateStr,HttpServletRequest request)throws Exception{
-		ModelAndView mav=new ModelAndView();
-		if(StringUtil.isEmpty(page)){
-			page="1";
-		}
-		PageBean pageBean=new PageBean(Integer.parseInt(page),10);
-		Map<String,Object> map=new HashMap<String,Object>();
-		map.put("start", pageBean.getStart());
-		map.put("size", pageBean.getPageSize());
-		map.put("typeId", typeId);
-		map.put("releaseDateStr", releaseDateStr);
-		List<Blog> blogList=blogService.list(map);
-		//设置预览图片,最多为3张
-		for(Blog blog:blogList){
-			List<String> imagesList=blog.getImagesList();
-			String blogInfo=blog.getContent();
-			Document doc=Jsoup.parse(blogInfo);
-			//查找图片元素
-			Elements jpgs=doc.select("img"); 
-			for(int i=0;i<jpgs.size();i++){
-				Element jpg=jpgs.get(i);
-				imagesList.add(jpg.toString());
-				if(i==2){
-					break;
-				}
-			}
-		}
-		mav.addObject("blogList", blogList);
-		StringBuffer param=new StringBuffer(); // 查询参数
-		if(StringUtil.isNotEmpty(typeId)){
-			param.append("typeId="+typeId+"&");
-		}
-		if(StringUtil.isNotEmpty(releaseDateStr)){
-			param.append("releaseDateStr="+releaseDateStr+"&");
-		}
-		mav.addObject("pageCode",PageUtil.genPagination(request.getContextPath()+"/index.html", blogService.getTotal(map), Integer.parseInt(page), 10, param.toString()));
-		mav.addObject("mainPage", "foreground/blog/list.jsp");
-		mav.addObject("pageTitle","JavaBlog");
-		mav.setViewName("mainTemp");
-		return mav;
-	}
-	
-	/**
-	 * 源码下载
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/aboutSite")
-	public ModelAndView download()throws Exception{
-		ModelAndView mav=new ModelAndView();
-		mav.addObject("mainPage", "foreground/system/aboutSite.jsp");
-		mav.addObject("pageTitle","JavaBlog");
-		mav.setViewName("mainTemp");
-		return mav;
-	}
+    @Resource
+    private BlogService blogService;
+
+
+    /**
+     * 请求主页
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/index")
+    public ModelAndView index(@RequestParam(value = "page", required = false) String page, @RequestParam(value = "typeId", required = false) String typeId, @RequestParam(value = "releaseDateStr", required = false) String releaseDateStr, HttpServletRequest request) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        if (StringUtil.isEmpty(page)) {
+            page = "1";
+        }
+        PageBean pageBean = new PageBean(Integer.parseInt(page), 10);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("start", pageBean.getStart());
+        map.put("size", pageBean.getPageSize());
+        map.put("typeId", typeId);
+        map.put("releaseDateStr", releaseDateStr);
+        List<Blog> blogList = blogService.list(map);
+        //设置预览图片,最多为3张
+        for (Blog blog : blogList) {
+            List<String> imagesList = blog.getImagesList();
+            String blogInfo = blog.getContent();
+            blog.setReleaseDateStr(TimeUtils.getTime(blog.getReleaseDate(), "yyyy年MM月dd日"));
+            Document doc = Jsoup.parse(blogInfo);
+            //查找图片元素
+            Elements jpgs = doc.select("img");
+            for (int i = 0; i < jpgs.size(); i++) {
+                Element jpg = jpgs.get(i);
+                imagesList.add(jpg.toString());
+                if (i == 2) {
+                    break;
+                }
+            }
+        }
+        mav.addObject("blogList", blogList);
+        StringBuffer param = new StringBuffer(); // 查询参数
+        if (StringUtil.isNotEmpty(typeId)) {
+            param.append("typeId=" + typeId + "&");
+        }
+        if (StringUtil.isNotEmpty(releaseDateStr)) {
+            param.append("releaseDateStr=" + releaseDateStr + "&");
+        }
+        mav.addObject("pageCode", PageUtil.genPagination(request.getContextPath() + "/index.html", blogService.getTotal(map), Integer.parseInt(page), 10, param.toString()));
+        mav.addObject("mainPage", "foreground/blog/list.jsp");
+        mav.addObject("pageTitle", "JavaBlog");
+        mav.setViewName("mainTemp");
+        return mav;
+    }
+
+    /**
+     * 源码下载
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/aboutSite")
+    public ModelAndView download() throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("mainPage", "foreground/system/aboutSite.jsp");
+        mav.addObject("pageTitle", "JavaBlog");
+        mav.setViewName("mainTemp");
+        return mav;
+    }
 }

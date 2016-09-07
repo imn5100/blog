c5a89bdd4923b292b61e63b5c5bc8e2365ed51f2
tuanzi <tuanzi@2dfire.com>
2016-09-07 17:15:23 +0800

    update lucene config

--- src/main/java/com/shaw/lucene/BlogIndex.java
+++ src/main/java/com/shaw/lucene/BlogIndex.java
@@ -26,146 +26,145 @@ import java.util.List;
 
 /**
  * 博客索引类
- * 
- * @author Administrator
  *
+ * @author Administrator
  */
 public class BlogIndex {
 
-	private Directory dir = null;
+    private Directory dir = null;
+    public static final String DEAFULT_PATH = "/lucene";
 
-	/**
-	 * 获取IndexWriter实例
-	 * 
-	 * @return
-	 * @throws Exception
-	 */
-	public IndexWriter getWriter() throws Exception {
-		dir = FSDirectory.open(Paths.get("/lucene"));
-		SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
-		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
-		IndexWriter writer = new IndexWriter(dir, iwc);
-		return writer;
-	}
+    /**
+     * 获取IndexWriter实例
+     *
+     * @return
+     * @throws Exception
+     */
+    public IndexWriter getWriter() throws Exception {
+        dir = FSDirectory.open(Paths.get(DEAFULT_PATH));
+        SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        IndexWriter writer = new IndexWriter(dir, iwc);
+        return writer;
+    }
 
-	/**
-	 * 添加博客索引
-	 * 
-	 * @param blog
-	 */
-	public void addIndex(Blog blog) throws Exception {
-		IndexWriter writer = getWriter();
-		Document doc = new Document();
-		doc.add(new StringField("id", String.valueOf(blog.getId()), Field.Store.YES));
-		doc.add(new TextField("title", blog.getTitle(), Field.Store.YES));
-		doc.add(new StringField("releaseDate", DateUtil.formatDate(new Date(), "yyyy-MM-dd"), Field.Store.YES));
-		doc.add(new TextField("content", blog.getContentNoTag(), Field.Store.YES));
-		writer.addDocument(doc);
-		writer.close();
-	}
+    /**
+     * 添加博客索引
+     *
+     * @param blog
+     */
+    public void addIndex(Blog blog) throws Exception {
+        IndexWriter writer = getWriter();
+        Document doc = new Document();
+        doc.add(new StringField("id", String.valueOf(blog.getId()), Field.Store.YES));
+        doc.add(new TextField("title", blog.getTitle(), Field.Store.YES));
+        doc.add(new StringField("releaseDate", DateUtil.formatDate(new Date(), "yyyy-MM-dd"), Field.Store.YES));
+        doc.add(new TextField("content", blog.getContentNoTag(), Field.Store.YES));
+        writer.addDocument(doc);
+        writer.close();
+    }
 
-	/**
-	 * 更新博客索引
-	 * 
-	 * @param blog
-	 * @throws Exception
-	 */
-	public void updateIndex(Blog blog) throws Exception {
-		IndexWriter writer = getWriter();
-		Document doc = new Document();
-		doc.add(new StringField("id", String.valueOf(blog.getId()), Field.Store.YES));
-		doc.add(new TextField("title", blog.getTitle(), Field.Store.YES));
-		doc.add(new StringField("releaseDate", DateUtil.formatDate(new Date(), "yyyy-MM-dd"), Field.Store.YES));
-		doc.add(new TextField("content", blog.getContentNoTag(), Field.Store.YES));
-		writer.updateDocument(new Term("id", String.valueOf(blog.getId())), doc);
-		writer.close();
-	}
+    /**
+     * 更新博客索引
+     *
+     * @param blog
+     * @throws Exception
+     */
+    public void updateIndex(Blog blog) throws Exception {
+        IndexWriter writer = getWriter();
+        Document doc = new Document();
+        doc.add(new StringField("id", String.valueOf(blog.getId()), Field.Store.YES));
+        doc.add(new TextField("title", blog.getTitle(), Field.Store.YES));
+        doc.add(new StringField("releaseDate", DateUtil.formatDate(new Date(), "yyyy-MM-dd"), Field.Store.YES));
+        doc.add(new TextField("content", blog.getContentNoTag(), Field.Store.YES));
+        writer.updateDocument(new Term("id", String.valueOf(blog.getId())), doc);
+        writer.close();
+    }
 
-	/**
-	 * 删除指定博客的索引
-	 * 
-	 * @param blogId
-	 * @throws Exception
-	 */
-	public void deleteIndex(String blogId) throws Exception {
-		IndexWriter writer = getWriter();
-		writer.deleteDocuments(new Term("id", blogId));
-		writer.forceMergeDeletes(); // 强制删除
-		writer.commit();
-		writer.close();
-	}
+    /**
+     * 删除指定博客的索引
+     *
+     * @param blogId
+     * @throws Exception
+     */
+    public void deleteIndex(String blogId) throws Exception {
+        IndexWriter writer = getWriter();
+        writer.deleteDocuments(new Term("id", blogId));
+        writer.forceMergeDeletes(); // 强制删除
+        writer.commit();
+        writer.close();
+    }
 
-	/**
-	 * 查询博客信息
-	 * 
-	 * @param q
-	 *            查询关键字
-	 * @return
-	 * @throws Exception
-	 */
-	public List<Blog> searchBlog(String q) throws Exception {
-		// 获得索引目录
-		dir = FSDirectory.open(Paths.get("/lucene"));
-		// 创建索引reader
-		IndexReader reader = DirectoryReader.open(dir);
-		// 创建索引Searcher
-		IndexSearcher is = new IndexSearcher(reader);
-		// 构建搜索条件
-		BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
-		// 创建中文分词解析
-		SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
-		QueryParser parser = new QueryParser("title", analyzer);
-		Query query = parser.parse(q);
-		QueryParser parser2 = new QueryParser("content", analyzer);
-		Query query2 = parser2.parse(q);
-		// or 作为并集查询。s
-		booleanQuery.add(query, BooleanClause.Occur.SHOULD);
-		booleanQuery.add(query2, BooleanClause.Occur.SHOULD);
+    /**
+     * 查询博客信息
+     *
+     * @param q 查询关键字
+     * @return
+     * @throws Exception
+     */
+    public List<Blog> searchBlog(String q) throws Exception {
+        // 获得索引目录
+        dir = FSDirectory.open(Paths.get(DEAFULT_PATH));
+        // 创建索引reader
+        IndexReader reader = DirectoryReader.open(dir);
+        // 创建索引Searcher
+        IndexSearcher is = new IndexSearcher(reader);
+        // 构建搜索条件
+        BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
+        // 创建中文分词解析
+        SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
+        QueryParser parser = new QueryParser("title", analyzer);
+        Query query = parser.parse(q);
+        QueryParser parser2 = new QueryParser("content", analyzer);
+        Query query2 = parser2.parse(q);
+        // or 作为并集查询。s
+        booleanQuery.add(query, BooleanClause.Occur.SHOULD);
+        booleanQuery.add(query2, BooleanClause.Occur.SHOULD);
 
-		// 查询100条记录
-		TopDocs hits = is.search(booleanQuery.build(), 100/*, sort*/);
+        // 查询100条记录
+        TopDocs hits = is.search(booleanQuery.build(), 100/*, sort*/);
 
-		QueryScorer scorer = new QueryScorer(query);
-		Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
-		SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<b><font color='red'>", "</font></b>");
-		Highlighter highlighter = new Highlighter(simpleHTMLFormatter, scorer);
-		highlighter.setTextFragmenter(fragmenter);
-		List<Blog> blogList = new ArrayList<Blog>();
+        QueryScorer scorer = new QueryScorer(query);
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
+        SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<b><font color='red'>", "</font></b>");
+        Highlighter highlighter = new Highlighter(simpleHTMLFormatter, scorer);
+        highlighter.setTextFragmenter(fragmenter);
+        List<Blog> blogList = new ArrayList<Blog>();
 
-		// 遍历搜索结果，构建blog
-		for (ScoreDoc scoreDoc : hits.scoreDocs) {
-			Document doc = is.doc(scoreDoc.doc);
-			Blog blog = new Blog();
-			blog.setId(Integer.parseInt(doc.get(("id"))));
-			String time = doc.get(("releaseDate"));
-			blog.setReleaseDateStr(time);
-			String title = doc.get("title");
-			String content = StringEscapeUtils.escapeHtml(doc.get("content"));
-			if (title != null) {
-				TokenStream tokenStream = analyzer.tokenStream("title", new StringReader(title));
-				String hTitle = highlighter.getBestFragment(tokenStream, title);
-				if (StringUtil.isEmpty(hTitle)) {
-					blog.setTitle(title);
-				} else {
-					blog.setTitle(hTitle);
-				}
-			}
-			if (content != null) {
-				TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(content));
-				String hContent = highlighter.getBestFragment(tokenStream, content);
-				if (StringUtil.isEmpty(hContent)) {
-					if (content.length() <= 200) {
-						blog.setContent(content);
-					} else {
-						blog.setContent(content.substring(0, 200));
-					}
-				} else {
-					blog.setContent(hContent);
-				}
-			}
-			blogList.add(blog);
-		}
-		Collections.sort(blogList);
-		return blogList;
-	}
+        // 遍历搜索结果，构建blog
+        for (ScoreDoc scoreDoc : hits.scoreDocs) {
+            Document doc = is.doc(scoreDoc.doc);
+            Blog blog = new Blog();
+            blog.setId(Integer.parseInt(doc.get(("id"))));
+            String time = doc.get(("releaseDate"));
+            blog.setReleaseDateStr(time);
+            String title = doc.get("title");
+            String content = StringEscapeUtils.escapeHtml(doc.get("content"));
+            if (title != null) {
+                TokenStream tokenStream = analyzer.tokenStream("title", new StringReader(title));
+                String hTitle = highlighter.getBestFragment(tokenStream, title);
+                if (StringUtil.isEmpty(hTitle)) {
+                    blog.setTitle(title);
+                } else {
+                    blog.setTitle(hTitle);
+                }
+            }
+            if (content != null) {
+                TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(content));
+                String hContent = highlighter.getBestFragment(tokenStream, content);
+                if (StringUtil.isEmpty(hContent)) {
+                    if (content.length() <= 200) {
+                        blog.setContent(content);
+                    } else {
+                        blog.setContent(content.substring(0, 200));
+                    }
+                } else {
+                    blog.setContent(hContent);
+                }
+            }
+            blogList.add(blog);
+        }
+        Collections.sort(blogList);
+        return blogList;
+    }
 }

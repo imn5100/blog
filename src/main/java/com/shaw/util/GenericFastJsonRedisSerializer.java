28677994e1b3bbbb0d8d577918f03393708a4d2a
imn5100 <imn5100_chu2@163.com>
2017-04-01 11:05:52 +0800

    fix fast json auto type bug

--- src/main/java/com/shaw/util/GenericFastJsonRedisSerializer.java
+++ src/main/java/com/shaw/util/GenericFastJsonRedisSerializer.java
@@ -1,6 +1,7 @@
 package com.shaw.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
@@ -15,6 +16,12 @@ import java.nio.charset.Charset;
 public class GenericFastJsonRedisSerializer implements RedisSerializer<Object> {
     static final byte[] EMPTY_ARRAY = new byte[0];
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    public static final ParserConfig AUTO_TYPE_PARSE_CONFIG;
+
+    static {
+        AUTO_TYPE_PARSE_CONFIG = new ParserConfig();
+        AUTO_TYPE_PARSE_CONFIG.setAutoTypeSupport(true);
+    }
 
 
     public byte[] serialize(Object t) throws SerializationException {
@@ -28,7 +35,6 @@ public class GenericFastJsonRedisSerializer implements RedisSerializer<Object> {
         if (bytes == null || bytes.length <= 0) {
             return null;
         }
-        String str = new String(bytes, DEFAULT_CHARSET);
-        return JSON.parseObject(str, Object.class);
+        return JSON.parseObject(new String(bytes, DEFAULT_CHARSET), Object.class, AUTO_TYPE_PARSE_CONFIG);
     }
 }

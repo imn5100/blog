596e7b06fb408a288fe0d8f8c7e6b17255ead62f
xiaowc <xiaowc@dxy.cn>
2017-04-12 10:31:42 +0800

    add deploy_jetty.sh

---
+++ src/test/deploy_jetty.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+cp -f /usr/local/jetty/webapps/root.war /usr/myfile/blog_bak.war;
+\cp -rf  /usr/myfile/jettyTmp/webapp/static/userImages/* /usr/myfile/userImagesBak
+sh /usr/local/jetty/bin/jetty.sh  stop;
+cp -f /usr/myfile/root.war /usr/local/jetty/webapps/root.war;
+sh /usr/local/jetty/bin/jetty.sh start
+path=/usr/myfile/jettyTmp/webapp/static/userImages/
+if  [ -d $path ];then
+ \cp -rf  /usr/myfile/userImagesBak/* $path
+else
+ mkdir $path
+ \cp -rf  /usr/myfile/userImagesBak/* $path
+fi

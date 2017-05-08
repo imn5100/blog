7c8b99cea4741ee047f5767d820815578d22daf7
imn5100 <imn5100_chu2@163.com>
2017-05-08 12:48:19 +0800

    send group email

--- src/test/java/com/shaw/test/Test.java
+++ src/test/java/com/shaw/test/Test.java
@@ -3,6 +3,7 @@ package com.shaw.test;
 import com.shaw.util.CodecUtils;
 import com.shaw.util.DesUtils;
 import com.shaw.util.EmailUtils;
+import com.shaw.util.PropertiesUtil;
 import org.apache.commons.lang.exception.ExceptionUtils;
 
 public class Test {
@@ -15,7 +16,7 @@ public class Test {
     }
 
     public static void simpleDecode() throws Exception {
-        String encodeStr = CodecUtils.getEncodeId(114);
+        String encodeStr = CodecUtils.getEncodeId(1154);
         System.out.println(encodeStr);
         int decodeId = CodecUtils.getDecodeId(encodeStr);
         System.out.println(decodeId);
@@ -23,10 +24,11 @@ public class Test {
 
     public static void sendEmail() {
         try {
-            int i = 0;
-            i = i / 0;
+            DesUtils.getDefaultInstance().decrypt("OOOOOOO");
         } catch (Exception ex) {
-            System.out.println(EmailUtils.sendEmail("Blog system exception", org.apache.commons.lang.exception.ExceptionUtils.getFullStackTrace(ex), ""));
+            String[] emails = PropertiesUtil.getConfiguration().getStringArray("email.subscriber");
+            System.out.println(emails);
+            System.out.println(EmailUtils.sendEmail("Blog system exception", ExceptionUtils.getFullStackTrace(ex), emails));
         }
 
 
